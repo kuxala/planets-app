68bc0918d3ee4f5344d499acda5016fa6ac25478
Roman Kukhalashvili <cyberdna12@gmail.com>
2024-02-06 19:06:21 +0400

    made top border animations

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,136 @@
 // import React from 'react'
 import { Link } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 import "./Header.css";
+import styled from "styled-components";
+
+const StyledMercury = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #419ebb;
+  }
+`;
+const StyledVenus = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #eda249;
+  }
+`;
+const StyledEarth = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #6d2ed5;
+  }
+`;
+const StyledMars = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #d14c32;
+  }
+`;
+
+const Styledjupiter = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #D83A34;
+  }
+`;
+const StyledSaturn = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #CD5120;
+  }
+`;
+const StyledUranus = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #1EC1A2;
+  }
+`;
+const StyledNeptune = styled.li`
+  padding-top: 32px;
+  color: #fff;
+  font-family: Spartan;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 25px; /* 227.273% */
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  height: 8vh;
+  &:hover {
+    border-top: 5px solid #2D68F0;
+  }
+`;
 
 function Header() {
   const [show, setShow] = useState<any>(false);
- 
-  
- return (
+  console.log(show)
+  return (
     <>
-    
       <nav>
         <h1>THE PLANETS</h1>
         <img
@@ -17,33 +138,35 @@ function Header() {
           width="30px"
           height="30px"
           className="menu"
-          onClick={() => {setShow(!show)}}
+          onClick={() => {
+            setShow(!show);
+          }}
         />
-        
+
         <ul>
           <Link to="mercury">
-            <li>MERCURY</li>
+            <StyledMercury>MERCURY</StyledMercury>
           </Link>
           <Link to="venus">
-            <li>VENUS</li>
+            <StyledVenus>VENUS</StyledVenus>
           </Link>
           <Link to="earth">
-            <li>EARTH</li>
+            <StyledEarth>EARTH</StyledEarth>
           </Link>
           <Link to="mars">
-            <li>MARS</li>
+            <StyledMars>MARS</StyledMars>
           </Link>
           <Link to="jupiter">
-            <li>JUPITER</li>
+            <Styledjupiter>JUPITER</Styledjupiter>
           </Link>
           <Link to="saturn">
-            <li>SATURN</li>
+            <StyledSaturn>SATURN</StyledSaturn>
           </Link>
           <Link to="uranus">
-            <li>URANUS</li>
+            <StyledUranus>URANUS</StyledUranus>
           </Link>
           <Link to="neptune">
-            <li>NEPTUNE</li>
+            <StyledNeptune>NEPTUNE</StyledNeptune>
           </Link>
         </ul>
       </nav>
